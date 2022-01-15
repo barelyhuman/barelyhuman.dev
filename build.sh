46fc0fbff876e4c57c50df043dc709824eadb96a
Siddharth <ahoy@barelyhuman.dev>
2022-01-15 15:25:12 +0530

    modular build script

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-curl -sf https://goblin.reaper.im/barelyhuman/statico@c7fe03387b5d4958029beb8a447344747a31dafc | sh
+goblin () {
+  curl -sf "https://goblin.reaper.im/$1@$2" | sh 
+}
+
+goblin barelyhuman/statico c7fe03387b5d4958029beb8a447344747a31dafc
+
 statico
