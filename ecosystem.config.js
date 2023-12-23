a5cfbffc0455df2291f51ccea5787b8b46c6ee0c
Reaper Gelera <ahoy@barelyhuman.dev>
2023-12-23 17:41:10 +0530

    ci: update env for prod deploys

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,27 @@
 module.exports = {
   apps: [
     {
-      name: 'barelyhuman-dev',
-      script: 'PORT=3123 node .output/server/index.mjs',
-      args: 'start',
+      name: "barelyhuman-dev",
+      script: ".output/server/index.mjs",
+      args: "start",
       instances: 1,
-      exec_mode: 'cluster',
+      exec_mode: "cluster",
       wait_ready: true,
       listen_timeout: 5000,
+      "env_production": {
+        "PORT": "3123",
+      },
     },
   ],
-  deploy:{
-    production : {
-      "user" : "root",
-      "host" : ["barelyhuman.dev"],
-      ref:"origin/main",
-      "repo":"git@github.com:barelyhuman/barelyhuman.dev.git",
-      path:"/apps/barelyhuman.dev",
-      "post-deploy":"source ~/.nvm/nvm.sh; nvm use; yarn; yarn build; pm2 startOrRestart ecosystem.config.js --env production"
-  }
-}
-}
+  deploy: {
+    production: {
+      "user": "root",
+      "host": ["barelyhuman.dev"],
+      ref: "origin/main",
+      "repo": "git@github.com:barelyhuman/barelyhuman.dev.git",
+      path: "/apps/barelyhuman.dev",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh; nvm use; yarn; yarn build; pm2 startOrRestart ecosystem.config.js --env production",
+    },
+  },
+};
